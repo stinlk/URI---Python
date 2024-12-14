21d1c2ea72acd8d1dfe1093cd6b28817f68a2d24
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-14 14:08:59 -0400

    Update pum.py

--- pum.py
+++ pum.py
@@ -1,18 +1,1 @@
-# Pum - 1142
 
-qtt = int(input()) # Quantidade de linhas que vai ser imprimido.
-comeco = 0 # Contagem dos numeros.
-saber = 0 # Saber quantidade de vezes q foi imprimido para imprimir PUM.
-lista = []
-for i in range(qtt):
-    comeco += 1
-    saber += 1
-
-    lista.append(comeco)
-    
-
-    if saber == 3:
-        saber = 0
-for i in range(len(lista)):
-    
-    print(lista[i],"PUM", end='')
