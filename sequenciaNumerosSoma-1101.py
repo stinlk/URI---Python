e7ec3b624d9f19eeb10c97568a7f465986080eb1
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-14 14:11:03 -0400

    Update sequenciaNumerosSoma-1101.py

--- sequenciaNumerosSoma-1101.py
+++ sequenciaNumerosSoma-1101.py
@@ -1,16 +1,1 @@
-# Sequencia de Numeros e Soma - 1101
-# Objetivo: Fazer a soma entre o intervalo de dois numeros, incluindo eles mesmos.
 
-lista = []
-
-while 1:
-    m, n = map(int, input().split()) # Maior e Menor
-    lista.append(m)
-    lista.append(n)
-
-    for i in range(lista[i],):
-
-
-    if m == 0 or n == 0:
-        break
-print(lista)
