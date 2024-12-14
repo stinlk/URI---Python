9b8076ca7711c97661348b2eda9e13e7a77ca290
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-14 14:07:57 -0400

    Update mes-1052.py

--- mes-1052.py
+++ mes-1052.py
@@ -2,7 +2,7 @@
 # Objetivo: Colocar um numero e saber qual seu mes em ingles.
 
 mes = int(input())
-lista_mes = ["auauau","January", "February", "March","April", "May", "June","July","August","September","October","November","December"]
+lista_mes = ["January", "February", "March","April", "May", "June","July","August","September","October","November","December"]
 for i in range(len(lista_mes)):
-    if i == mes:
+    if i+1 == mes:
         print(lista_mes[i])
