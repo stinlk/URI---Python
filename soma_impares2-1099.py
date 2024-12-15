5605d66fb0ee4ed5039bc931ef46e890ae2d0a3e
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-15 13:14:06 -0400

    Update soma_impares2-1099.py

--- soma_impares2-1099.py
+++ soma_impares2-1099.py
@@ -1,2 +1,20 @@
 # Soma de impares consecutivos 2 - 1099
-# Compreender como funciona a soma dentro desse intervalo.
+# Objetivo: fazer uma soma entre um intervalo de dois numeros, mas apenas a soma dos impares.
+
+qtt = int(input())
+
+for i in range(qtt):
+    soma = 0
+    aux = 0
+
+    x, y = map(int, input().split())
+    if x > y:
+        aux = y
+        y = x
+        x = aux
+        
+    for j in range(x+1,y):
+        if j % 2 != 0:
+            soma += j
+
+    print(soma)
