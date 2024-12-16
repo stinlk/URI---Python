b67130fd0ded7547e0cf089bf5244c1cfc5508ca
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-16 18:54:07 -0400

    Update pum.py

--- pum.py
+++ pum.py
@@ -1,1 +1,19 @@
+# PUM - 1142
+# Imprimir 3x uma sequencia e imprimir PUM e continuar a sequencia da onde parou anteriormente.
 
+qtt = int(input()) # Quantidade de vezes que a linha vai ser imprimida.
+linhas = 0
+i = 0
+cont = 0
+while 1:
+    i+= 1
+    print(i, end=" ") # Imprimir os numeros.
+    cont += 1
+    if cont == 3:
+        print("PUM\n", end="") # Imprimir 'PUM' dps que se repetir 3x.
+        i += 1
+        cont = 0
+        linhas += 1
+
+    if linhas == qtt:
+        break
