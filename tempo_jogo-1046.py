6abaa9a9c7a2eaa57bf02676e1f311987fa2cb6f
Sinclair <141573344+stinlk@users.noreply.github.com>
2024-12-21 11:18:40 -0400

    Update tempo_jogo-1046.py

--- tempo_jogo-1046.py
+++ tempo_jogo-1046.py
@@ -1,5 +1,5 @@
-# Tempo de jogo - 1046
-# Objetivo: Saber quanto tempo durou um jogo.
+# Tempo de jogo - 1047
+# Objetivo: Saber quanto tempo durou um jogo em minutos.
 # Lembre-se o formato das horas é 24h
 
 # Hora de incio e fim
